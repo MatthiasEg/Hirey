c07efd027eb395390143f04c383534782a075836
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-11-02 21:29:54 +0100

    Implemented connection to IPFS and uploading files

--- pages/upload.jsx
+++ pages/upload.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@material-ui/core/Box'
+import ipfs from '../lib/IPFSClient'
 
 const Upload = () => {
-  return <h1>Upload</h1>
+  const [buffer, setBuffer] = useState(null)
+  const [lastUploadedDocumentHash, setLastUploadedDocumentHash] = useState('')
+
+  const captureFile = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    if (!file) return
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      setBuffer(Buffer.from(reader.result))
+    }
+  }
+
+  // Example hash: QmSAdbek1DDb91BM8no29LeRxapusH72pmMZWs8zokGt6p
+  // Example url: https://ipfs.infura.io/ipfs/QmSAdbek1DDb91BM8no29LeRxapusH72pmMZWs8zokGt6p
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (buffer) {
+      ipfs.add(buffer).then((response) => {
+        console.log('ipfs result: ', response.path)
+        setLastUploadedDocumentHash(response.path)
+      })
+    }
+  }
+
+  return (
+    <Box component='div'>
+      <h1>Upload</h1>
+      <form onSubmit={onSubmit}>
+        <input type='file' onChange={captureFile} />
+        <input type='submit' />
+      </form>
+      {lastUploadedDocumentHash ? (
+        <a href={`https://ipfs.infura.io/ipfs/${lastUploadedDocumentHash}`}>
+          Link to last uploaded file
+        </a>
+      ) : (
+        <></>
+      )}
+    </Box>
+  )
 }
 
 export default Upload
