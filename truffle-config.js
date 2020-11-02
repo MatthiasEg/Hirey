f9342cc02e75d679ca8bd0acc52cc07b4bb274ae
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-11-02 22:49:36 +0100

    truffle config adjusted

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 /* eslint-disable max-len */
+
+require('babel-register')
+require('babel-polyfill')
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -73,6 +77,11 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -80,18 +89,21 @@ module.exports = {
     // timeout: 100000
   },
 
+  contracts_directory: './contracts/',
+  contracts_build_directory: './build/',
+
   // Configure your compilers
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
       //  evmVersion: "byzantium"
       // }
     },
   },
-};
+}
