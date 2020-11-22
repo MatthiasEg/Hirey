c22c4a1596443c9e1102be309bafc3eaff6feaad
Lukas Ingold <technik@ingold.li>
2020-11-22 18:20:54 +0100

    textfield values in correct json-format

--- pages/university.jsx
+++ pages/university.jsx
@@ -1,51 +1,100 @@
 import React from 'react'
-
 // imports for TextFields
+import Box from '@material-ui/core/Box'
 import TextField from '@material-ui/core/TextField';
+//imports for upload (unused atm)
+import { useState } from 'react'
+import ipfs from '../lib/IPFSClient'
+import { useUser } from '../context/UserProvider'
+import { useContract } from '../context/ContractProvider'
 
 const University = () => {
+
+  // States from Upload?
+  const [buffer, setBuffer] = useState(null)
+  const [hash, setHash] = useState('')
+  const [uploadSuccessful, setUploadSuccessful] = useState(false)
+  const { user } = useUser()
+  const { contract } = useContract()
+
+  // States 
+  const [edu, setEdu] = useState('')
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [author, setAuthor] = useState('Hochschule Luzern')
+  const [date, setDate] = useState('')
+
+  const captureFile = (event) => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    if (!file) return
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      setBuffer(Buffer.from(reader.result))
+    }
+  }
+
+  // Anpassen für JSON
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(JSON.stringify({
+      "type": edu,
+      "title": title,
+      "description": desc,
+      "autor": author,
+      "publishDate": date,
+      //"documents": ["filebuffer"]
+    }))
+
+    if (buffer) {
+      ipfs.add(buffer).then((response) => {
+        setHash(response.path)
+        contract.methods
+          .set(response.path)
+          .send({
+            from: user.address,
+          })
+          .then(() => {
+            setUploadSuccessful(true)
+          })
+      })
+    }
+
+    alert ("Hallo")
+  }
+
+
   return (
-    <form noValidate autoComplete="off">
-    <div>
+    <Box component='div'>
       <h1>University</h1>
       <h3>Information</h3>
-      <TextField required id="standard-required" label="Ausbildungstyp" fullWidth defaultValue="Bachelor" />
-      &nbsp;
-      <TextField required id="standard-required" label="Titel" fullWidth defaultValue="Bachelor of Science / Informatik" />
-      &nbsp;
-      <TextField id="standard-basic" label="Beschreibung" fullWidth defaultValue="bla bli bla blup" />
-      &nbsp;
-      <TextField disabled id="standard-disabled" label="Autor" fullWidth defaultValue="Hochschule Luzern Informatik" />
-      &nbsp;
-      <TextField required id="standard-required" label="Publizierungsdatum" fullWidth defaultValue="01.01.1990" />
-      
-      &nbsp;&nbsp;
-      
-      <h3>Certificate</h3>
-      <form>
-        <input type='file' />
-      </form>
-
-      &nbsp;&nbsp;
-      &nbsp;&nbsp;
-
-      <h2>Upload</h2>
-      <form>
+      <form onSubmit={onSubmit}>
+        <TextField required id="standard-required1" label="Ausbildungstyp" fullWidth value={edu} onChange={event => setEdu(event.target.value)} />
+        &nbsp;
+        <TextField required id="standard-required2" label="Titel" fullWidth value={title} onChange={event => setTitle(event.target.value)}  />
+        &nbsp;
+        <TextField id="standard-basic1" label="Beschreibung" fullWidth value={desc} onChange={event => setDesc(event.target.value)}  />
+        &nbsp;
+        <TextField disabled id="standard-disabled1" label="Autor" fullWidth value={author} onChange={event => setAuthor(event.target.value)}  />
+        &nbsp;
+        <TextField required id="standard-required3" label="Publizierungsdatum" fullWidth value={date} onChange={event => setDate(event.target.value)}  />
+        &nbsp;&nbsp;
+        <h3>Certificate</h3>
+        {/* <input required id="input1" type='file' onChange={captureFile}/> */}
+        <input id="input1" type='file' onChange={captureFile}/>
+        &nbsp;&nbsp;
+        <h2>Upload</h2>
         <input type='submit' value='Auf zur Blockchain' />
-      </form>
-  </div>
-  </form>
+    </form>
+    {uploadSuccessful ? (
+        <Typography>Upload was successful!</Typography>
+      ) : (
+        <></>
+      )}
+  </Box>
   )
-
   
 }
-
-// "type": "ausbildung",
-// "title": "bla",
-// "description": "bla",
-// "autor": "bla",
-// "publishDate": "bla",
-// "documents": ["filebuffer"]
-
 
 export default University
