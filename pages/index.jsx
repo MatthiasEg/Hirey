19b97be1bcf9be94f493634a1f82ecd2e6c75140
Matthias Egli <matthias.egli@stud.hslu.ch>
2020-11-27 14:10:24 +0100

    homepaged centered + slogan removed

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,16 +7,20 @@ const Home = () => {
   const logoWidth = 300
   return (
     <>
-      <Grid container direction='column' spacing={2}>
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        alignContent='center'
+        justify='center'
+        spacing={2}
+      >
         <Grid item>
           <Image
             src='/logo.png'
             width={logoWidth}
             height={logoWidth * 1.085514834205}
           />
-        </Grid>
-        <Grid item>
-          <Typography variant='h6'>slogan und mehr blablaaa</Typography>
         </Grid>
       </Grid>
     </>
