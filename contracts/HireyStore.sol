26655761796a7beccc77a48c52143fe45572622c
Manuel.Steinegger <steinegger.manuel@gmail.com>
2020-11-29 15:15:09 +0100

    only allow sharing existing documents

--- contracts/HireyStore.sol
+++ contracts/HireyStore.sol
@@ -27,29 +27,36 @@ contract HireyStore {
     contractCreatorAddress = msg.sender;
   }
 
-  // Bildungsinstitution oder Arbeitgeber kann einen Document-Hash für einen Bewerber hinzufügen
+  // Eine Bildungsinstitution oder ein Arbeitgeber kann einen _cvRecordHash für einen Bewerber hinzufügen. Dieser _cvRecordHash zeigt auf eine Datei (auf dem IPFS),
+  // welche vom Sender signiert und mit dem Public-Key des Bewerbers verschlüsselt wurde. Der Hash wurde mit dem HIREY-System Public-Key verschlüsselt. 
   function storeCvRecordFor(address _applicantAccount, string memory _cvRecordHash) public {
     cvRecordHashes[_applicantAccount].push(CVRecord({author: msg.sender, documentHash: _cvRecordHash}));
     nbrOfCvRecordHashes[_applicantAccount] = nbrOfCvRecordHashes[_applicantAccount] + 1;
   }
 
-  // Bewerber kann seinen CVDocument-Hash für sich selber speicher
+  // Ein Bewerber kann einen _cvDocumentHash für sich selbst speichern. Die Datei auf IPFS enthält mehere cvRecords. 
+  // Die cvRecords wurden vom ursprünglichen Sender signiert. Gespeichert ist das cvDocument hier ebenfalls mit dem Public-Key des Bewerbers.
+  // Der Hash wurde mit dem HIREY-System Public-Key verschlüsselt.   
   function storeCVDocument(string memory _cvDocumentHash) public {
     cvDocumentHashes[msg.sender].push(CVDocument({author: msg.sender, documentHash: _cvDocumentHash, isUnlocked: true}));
     nbrOfCvDocumentHashes[msg.sender] = nbrOfCvDocumentHashes[msg.sender] + 1;
   }
 
-  // Bewerber kann seinen CV-Hash für eine HR-Abteilung speichern. Die HR-Abteilung muss den Eintrag dann mit Eth freischalten (unlock=True).
-  // Beim Teilen gibt er den neuen cvDocumentHash und den index des alten Documentes mit. 
+  // Bewerber kann seine CVDocumente für HR-Abteilungen freigeben. Die HR-Abteilung muss den Eintrag dann mit Eth freischalten (unlock=True).
+  // Beim Teilen gibt er den _cvDocumentHash des freizugebenden CVDocumentes mit. Dieses Dokument enthält die von den Firmen und Schulen signierten
+  // cvRecords. Die cvRecords und das gesamte Dokument wurde mit dem Public-Key der HR-Abteilung verschlüsselt. Der Hash wurde mit dem HIREY-System Public-Key verschlüsselt. 
   function shareCVDocument(address _targetAccount, uint cvDocumentIndex, string memory _cvDocumentHash) public {
-    require(_targetAccount != msg.sender);
+    require(_targetAccount != msg.sender); // Um ein Dokument für dich selber zu speichern muss mann storeCVDocument verwenden
+    require(bytes(cvDocumentHashes[msg.sender][cvDocumentIndex].documentHash).length  > 0); // das Dokument dass man freigibt muss auch existieren 
+
     cvDocumentHashes[_targetAccount].push(CVDocument({author: msg.sender, documentHash: _cvDocumentHash, isUnlocked: false}));
     sharedTo[msg.sender][cvDocumentIndex].push(_targetAccount);
     nbrOfSharedTo[msg.sender][cvDocumentIndex] = nbrOfSharedTo[msg.sender][cvDocumentIndex] + 1;
     nbrOfCvDocumentHashes[_targetAccount] = nbrOfCvDocumentHashes[_targetAccount] + 1;
   }
 
-  // CV-Hashes welche für eine HR-Abteilung hinzugefügt wurden müssen zuerst freigeschaltet werden. Gemäss unserem Konzept kostet das die HR-Abteilung aber etwas. 
+  // Die HR-Abteilungen bezahlen für das Anschauen der CVDokumente einmalig. Daher müssen sie das Dokument initial freischalten. 
+  // Nur freigeschaltete CVDocument-Hashes werden durch den HIREY-SYSTEM Private-Key entschlüsselt. 
   function unlockCVDocument(uint cvDocumentIndex) public payable {
     require(!cvDocumentHashes[msg.sender][cvDocumentIndex].isUnlocked);
     require(msg.value >= 30000);
